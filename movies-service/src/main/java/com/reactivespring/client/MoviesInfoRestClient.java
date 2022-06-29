ee4dfd54686d3f89c4027ff4b85627199b33129c
Santiago Rodriguez <santiago.rodriguezg@usantoto.edu.co>
2022-06-29 17:45:04 -0500

    completed up to 27 section

---
+++ movies-service/src/main/java/com/reactivespring/client/MoviesInfoRestClient.java
@@ -0,0 +1,55 @@
+package com.reactivespring.client;
+
+import com.reactivespring.domain.MovieInfo;
+import com.reactivespring.exception.MoviesInfoClientException;
+import com.reactivespring.exception.MoviesInfoServerException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import javax.validation.Valid;
+
+@Component
+@Slf4j
+public class MoviesInfoRestClient {
+
+    @Autowired
+    private WebClient webClient;
+
+    @Value("${restClient.moviesInfoUrl}")
+    private String moviesInfoUrl;
+
+    public Mono<MovieInfo> retrieveMovieInfo(String movieId){
+
+        var url = moviesInfoUrl.concat("/{id}");
+        return webClient
+                .get()
+                .uri(url, movieId)
+                .retrieve()
+                .onStatus(HttpStatus::is4xxClientError, clientResponse -> {
+                    log.info("Status code is : {}", clientResponse.statusCode().value());
+                    if(clientResponse.statusCode().equals(HttpStatus.NOT_FOUND)){
+                        return Mono.error(new MoviesInfoClientException("there is no MovieInfo Available for the passed Id: "+ movieId,
+                                clientResponse.statusCode().value()));
+                    }
+                    return clientResponse.bodyToMono(String.class)
+                            .flatMap(responseMessage -> Mono.error(new MoviesInfoClientException(
+                                    responseMessage, clientResponse.statusCode().value()
+                            )) );
+                })
+                .onStatus(HttpStatus::is5xxServerError, clientResponse -> {
+                    log.info("Status code is : {}", clientResponse.statusCode().value());
+
+                    return clientResponse.bodyToMono(String.class)
+                            .flatMap(responseMessage -> Mono.error(new MoviesInfoServerException(
+                                    "Server Exception in MoviesInfoService " + responseMessage)));
+                })
+                .bodyToMono(MovieInfo.class)
+                .log();
+
+    }
+}
--- movies-service/src/main/java/com/reactivespring/controller/MoviesController.java
+++ movies-service/src/main/java/com/reactivespring/controller/MoviesController.java
@@ -1,5 +1,37 @@
 package com.reactivespring.controller;
 
-@
+import com.reactivespring.client.MoviesInfoRestClient;
+import com.reactivespring.client.ReviewsRestClient;
+import com.reactivespring.domain.Movie;
+import com.reactivespring.exception.ReviewsClientException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/v1/movies")
 public class MoviesController {
+
+    @Autowired
+    private MoviesInfoRestClient moviesInfoRestClient;
+
+    @Autowired
+    private ReviewsRestClient reviewsRestClient;
+
+    @GetMapping("/{id}")
+    public Mono<Movie> retrieveMovieById(@PathVariable("id") String movieId){
+
+      return   moviesInfoRestClient.retrieveMovieInfo(movieId)
+                .flatMap(movieInfo -> {
+                    var reviewsListMono = reviewsRestClient.retrieveReviews(movieId)
+                            .collectList();
+
+                  return  reviewsListMono.map(reviews -> new Movie(movieInfo, reviews));
+
+                });
+
+    }
 }
